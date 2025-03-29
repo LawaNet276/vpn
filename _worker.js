e598b487d0831feefcdd6511e92bbe66156da7aa
LawaNet276 <140466731+LawaNet276@users.noreply.github.com>
2025-03-29 09:44:05 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 import { connect } from "cloudflare:sockets";
 
 // Variables
-const rootDomain = "lucilfer27061999.workers.dev"; // Ganti dengan domain utama kalian
+const rootDomain = "lawanet.my.id"; // Ganti dengan domain utama kalian
 const serviceName = "vpn"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
